13bb615cf070115a4fc2bc65cfc444c83be9055c
wey <weypro@users.noreply.github.com>
2022-10-02 23:01:29 +0800

    fix: cannot find pages on Windows
    Reference: https://github.com/mrmlnc/fast-glob#how-to-write-patterns-on-windows

--- dist/utils/get-html-pages.js
+++ dist/utils/get-html-pages.js
@@ -2,13 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = require("path");
 const fast_glob_1 = require("fast-glob");
+const normalize = require('normalize-path');
 /**
  * Gets html pages paths from `pagesDir`.
  * @param pagesDir root directory of pages
  * @param ignore An array of glob patterns to exclude matches.
  */
 function getHtmlPages(pagesDir, ignore) {
-    const path = (0, path_1.resolve)(pagesDir, '**', '*.html');
+    const path = normalize((0, path_1.resolve)(pagesDir, '**', '*.html'));
     const pages = (0, fast_glob_1.sync)(path, {
         ignore
     });
