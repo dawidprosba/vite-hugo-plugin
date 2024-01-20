58ff891eafedb086ad4869745121a0efe0606e2a
Dawid Prośba <david.prosba@gmail.com>
2024-01-20 13:03:02 +0100

    Update to Vite ^5, Add tsup for easier library building (#11)

--- dist/utils/index.d.ts
+++
@@ -1,3 +0,0 @@
-import getHtmlPages from "./get-html-pages";
-import getHugoConfig from "./get-hugo-config";
-export { getHtmlPages, getHugoConfig };
---
+++ tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    entry: ["src/index.ts"],
+    format: ["cjs", "esm"],
+    dts: true,
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    minify: false,
+    footer: {
+        js: "module.exports = module.exports.default;",
+    }
+});
